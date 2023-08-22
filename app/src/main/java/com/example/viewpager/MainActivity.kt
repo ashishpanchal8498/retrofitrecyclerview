5f23bb5b1c26bdbecfcafd9429a1eaa76bbf063b
ashish.panchal <ashish,panchal@rigelnetworks.com>
2023-08-22 13:56:14 +0530

    Friends Retrofit Both Fragments ViewPager Recycler View

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.example.viewpager
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,22 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     private val tabTitle = arrayOf("Friends", "Groups")
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: RecyclerAdapter
-    private var data: List<UsersItem> = emptyList()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        recyclerView = findViewById(R.id.recyclerView)
-
-        adapter = RecyclerAdapter(this, data)
-
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = adapter
-        getAllData()
 
         val pager = findViewById<ViewPager2>(R.id.viewPager)
         val tabular = findViewById<TabLayout>(R.id.tabLayout)
@@ -43,34 +22,5 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tabular, pager) { tab, position ->
             tab.text = tabTitle[position]
         }.attach()
-    }
-
-    private fun getAllData() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(baseurl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiInterface::class.java)
-
-        val retroData = retrofit.getData()
-
-        retroData.enqueue(object : Callback<List<UsersItem>> {
-            override fun onResponse(
-                call: Call<List<UsersItem>>,
-                response: Response<List<UsersItem>>
-            ) {
-                val data = response.body()!!
-
-                adapter = RecyclerAdapter(baseContext, data)
-
-                recyclerView.adapter = adapter
-
-                Log.d("data", data.toString())
-            }
-
-            override fun onFailure(call: Call<List<UsersItem>>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
     }
 }
--- app/src/main/java/com/example/viewpager/fragments/FriendsFragment.kt
+++ app/src/main/java/com/example/viewpager/fragments/FriendsFragment.kt
@@ -1,19 +1,69 @@
 package com.example.viewpager.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.viewpager.ApiInterface
 import com.example.viewpager.R
+import com.example.viewpager.RecyclerAdapter
+import com.example.viewpager.UsersItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class FriendsFragment : Fragment() {
 
+    private var baseurl = "https://api.github.com"
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerAdapter
+    private var data: List<UsersItem> = emptyList()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+        val view = inflater.inflate(R.layout.fragment_friends, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        adapter = RecyclerAdapter(requireContext(), data)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+
+        getAllData()
+
+        return view
+    }
+
+
+    private fun getAllData() {
+        val retrofit =
+            Retrofit.Builder().baseUrl(baseurl).addConverterFactory(GsonConverterFactory.create())
+                .build().create(ApiInterface::class.java)
+
+        val retroData = retrofit.getData()
+
+        retroData.enqueue(object : Callback<List<UsersItem>> {
+            override fun onResponse(
+                call: Call<List<UsersItem>>, response: Response<List<UsersItem>>
+            ) {
+                val data = response.body()!!
+
+                adapter = RecyclerAdapter(requireContext(), data)
+
+                recyclerView.adapter = adapter
+
+                Log.d("data", data.toString())
+            }
+
+            override fun onFailure(call: Call<List<UsersItem>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
